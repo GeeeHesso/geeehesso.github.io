3eec80ab20616218a1d21a8ed684e5c1b67303db
David T <david.tauxe@hevs.ch>
2018-04-23 12:03:16 +0200

    Solved mixed content error. http->https

--- inthenews/display.js
+++ inthenews/display.js
@@ -1,6 +1,6 @@
 var display_element = document.getElementById('news-results');
 
-   $.getJSON('news.json', function(data) {
+   $.getJSON('{{site.baseurl}}/news.json', function(data) {
     var appendString = '';
        $.each(data.news, function(i, f) {
           appendString += '<li style="margin: 15px 0"><a class=link href="'+ f.url+'" target="_blank">' +f.date+' / '+f.display+'</a>';

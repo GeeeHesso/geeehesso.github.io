74de4073294becbaf6f1c6ac3ddb7fb106e2d66e
David T <david.tauxe@hevs.ch>
2018-04-23 13:39:51 +0200

    debug

--- inthenews/display.js
+++ inthenews/display.js
@@ -5,6 +5,7 @@ console.log("Test")
    $.getJSON('{{site.baseurl}}/news.json', function(data) {
     var appendString = '';
        $.each(data.news, function(i, f) {
+	  console.log(f.display)
           appendString += '<li style="margin: 15px 0"><a class=link href="'+ f.url+'" target="_blank">' +f.date+' / '+f.display+'</a>';
      });
     display_element.innerHTML = appendString;
--- publications/display.js
+++ publications/display.js
@@ -1,6 +1,6 @@
 var display_element = document.getElementById('publications-results');
 
-   $.getJSON('{{site.baseurl}}/pub.json', function(data) {
+   $.getJSON('pub.json', function(data) {
     var appendString = '';
        $.each(data.publication, function(i, f) {
           appendString += '<li style="margin: 15px 10px"><p style="text-align: justify"><a class=link href="'+ f.url+'" target="_blank">' +f.display+'</a></p>';

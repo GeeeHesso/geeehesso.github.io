1670180a677e4ff147d8f927043db3c8abc59cb6
David T <david.tauxe@hevs.ch>
2018-05-15 18:01:43 +0200

    Banner for blog. Slighty changed the layout

--- inthenews/display.js
+++ inthenews/display.js
@@ -1,11 +1,8 @@
 var display_element = document.getElementById('newsresults');
 
-console.log("Test")
-
    $.getJSON('news.json', function(data) {
     var appendString = '';
        $.each(data.news, function(i, f) {
-	  console.log(f.display)
           appendString += '<li style="margin: 15px 0"><a class=link href="'+ f.url+'" target="_blank">' +f.date+' / '+f.display+'</a>';
      });
     display_element.innerHTML = appendString;

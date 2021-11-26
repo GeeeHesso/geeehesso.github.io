2050b4d0640a0fb23366d8d7eeab13fe99d19f24
Julian Fritzsch <me@julianfritzsch.de>
2021-11-26 11:33:33 +0100

    Added latest paper to What's New

--- whatnew.js
+++ whatnew.js
@@ -13,34 +13,34 @@ var ref_pubs = '';
 var ref_pubs = '';
 var ref_pubs = '';
 
-$.getJSON('/search/pub.json', function(data) {
-    str_pubs = data.publication['p_00'].display
-    ref_pubs = data.publication['p_00'].url
+$.getJSON('/search/pub.json', function (data) {
+	str_pubs = data.publication['p_00'].display
+	ref_pubs = data.publication['p_00'].url
 	el_pubs.innerHTML = str_pubs;
 	el_pubs.href = ref_pubs;
 	el_pubs.target = '_blank';
 });
 
 
-$.getJSON('/search/news.json', function(data) {
-    str_news = data.news['n_00'].display
-    ref_news = data.news['n_00'].url
+$.getJSON('/search/news.json', function (data) {
+	str_news = data.news['n_00'].display
+	ref_news = data.news['n_00'].url
 	el_news.innerHTML = str_news;
 	el_news.href = ref_news;
 	el_news.target = '_blank';
 });
 
 
-$.getJSON('/search/pres.json', function(data) {
-    str_talk = data.presentations['p_00'].display
-    ref_talk = data.presentations['p_00'].url
+$.getJSON('/search/pres.json', function (data) {
+	str_talk = data.presentations['p_00'].display
+	ref_talk = data.presentations['p_00'].url
 	el_talk.innerHTML = str_talk;
 	el_talk.href = ref_talk;
 	el_talk.target = '_blank';
 });
 
-str_blog=window.store[Object.keys(window.store)[0]].title
-ref_blog=window.store[Object.keys(window.store)[0]].url
+str_blog = window.store[Object.keys(window.store)[0]].title
+ref_blog = window.store[Object.keys(window.store)[0]].url
 
 el_blog.innerHTML = str_blog;
 el_blog.href = ref_blog;
